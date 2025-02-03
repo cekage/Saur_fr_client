d57b07c31b51323bae88e8fe25b7e676ef5a6274
k <k@cekage.net>
2025-02-03 19:58:44 +0000

    Disponibilité de SaurApiError

--- saur_client/__init__.py
+++ saur_client/__init__.py
@@ -7,4 +7,4 @@ except ImportError:
         "0.0.0+unknown"  # Pour les cas où le package n'est pas installé via setuptools
     )
 
-from .saur_client import SaurClient
+from .saur_client import SaurApiError, SaurClient
