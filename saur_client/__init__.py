fa108c6a3e2068d795bad67bc899b9eb19a36f9a
k <k@cekage.net>
2025-02-08 21:37:08 +0000

    fixing mypy type warnings

--- saur_client/__init__.py
+++ saur_client/__init__.py
@@ -6,6 +6,6 @@ except ImportError:
     # Pour les cas où le package n'est pas installé via setuptools
     __version__ = "0.0.0+unknown"
 
-from .saur_client import SaurApiError, SaurClient
+from .saur_client import SaurApiError, SaurClient, SaurResponse
 
 __all__ = ["SaurApiError", "SaurClient", "SaurResponse"]
