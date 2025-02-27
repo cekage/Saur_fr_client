ad4e19ca6a548ba1c626b21c6dc29fe03eb6519a
Cekage Dave <github@cekage.net>
2025-02-27 18:58:13 +0000

    chore: bump version to 0.3.2

--- saur_client/_version.py
+++ saur_client/_version.py
@@ -4,12 +4,18 @@
 __all__ = ["__version__", "__version_tuple__", "version", "version_tuple"]
 
 TYPE_CHECKING = False
-VERSION_TUPLE = tuple[int | str, ...] if TYPE_CHECKING else object
+if TYPE_CHECKING:
+    from typing import Tuple
+    from typing import Union
+
+    VERSION_TUPLE = Tuple[Union[int, str], ...]
+else:
+    VERSION_TUPLE = object
 
 version: str
 __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.3.1"
-__version_tuple__ = version_tuple = (0, 3, 1)
+__version__ = version = '0.3.2'
+__version_tuple__ = version_tuple = (0, 3, 2)
